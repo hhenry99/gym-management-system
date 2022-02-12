48155dac1ae08665624dfd9e23dc8f4e2522e5b9
hhenry99 <henrygl450@yahoo.com>
2022-02-12 15:52:03 -0500

    Add & display equipment
    Functionality to add and view equip

---
+++ GGG_System/add-equip.php
@@ -0,0 +1,71 @@
+<?php include('partials/header.php');?>
+
+<div class="main-content">
+    <div class="header">
+        <h1 class = "txt-center">Equipment</h1>
+        <p class = "txt-center">
+            <?php 
+                if(isset($_SESSION['add']))
+                {
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+            ?>
+        </p>
+
+    </div>
+
+    <div class="info">
+        <form action="" method = "POST">
+            <table class = "tbl-30">
+                <tr>
+                    <td>Name</td>
+                    <td><input type="text" name = "name"></td>
+                </tr>
+                <tr>
+                    <td>Condition</td>
+                    <td>
+                        <!-- <input type="text" name = "cond"> -->
+                        <select name="cond">
+                            <option value="good">Good</option>
+                            <option value="dirty">Dirty</option>
+                            <option value="broken">Broken</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan = "2">
+                        <input type="submit" value="Add Equip." class = "btn-primary" name = "submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+        <?php
+            if(isset($_POST['submit']))
+            {
+                $name = $_POST['name'];
+                $cond = $_POST['cond'];
+
+                $sql = "INSERT INTO equipment SET
+                        name = '$name',
+                        cond = '$cond';";
+
+                $res = mysqli_query($conn,$sql);
+
+                if($res == true)
+                {
+                    $_SESSION['add'] = "Equipment added successfully";
+                    header('location:'.SITEURL.'manage-equipment.php');
+                }
+                else
+                {
+                    $_SESSION['add'] = "fail to add equipment";
+                    header('location:'.SITEURL.'add-equip.php');
+                }
+            }
+        ?>
+    </div>
+
+</div>
+<?php include('partials/footer.php');?>
--- GGG_System/manage-equipment.php
+++ GGG_System/manage-equipment.php
@@ -1,15 +1,68 @@
-<?php include('header.php');?>
+<?php include('partials/header.php');?>
 
 
-<div class="main-content">
-            <div class="header">
-                <h1>Manage Admin</h1>
-            </div>
-
-            <div class="info">
-                <p>test</p>
-                <p>test</p>
-            </div>
+    <div class="main-content">
+        <div class="header">
+            <h1 class = "txt-center">Manage Equipment</h1>
+            <p class = "txt-center">
+                <?php 
+                if(isset($_SESSION['add']))
+                {
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+                ?>
+            </p>
         </div>
 
-<?php include("footer.php");?>
+        <div class="info">
+            <a href="<?php echo SITEURL;?>add-equip.php"><button class = "btn-primary">Add Equipment</button></a>
+            <table class = "tbl-full txt-left">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Condition</th>
+                    <th>Actions</th>
+                </tr>
+                <?php
+                    $sql = "SELECT * FROM equipment;";
+                    
+                    $res = mysqli_query($conn,$sql);
+                    
+                    $count = mysqli_num_rows($res);
+
+                    if($count > 0)
+                    {
+                        while($row = mysqli_fetch_assoc($res))
+                        {
+                            $id = $row['equipment_id'];
+                            $name = $row['name'];
+                            $condition = $row['cond'];
+                            
+                            ?>
+                                <tr>
+                                    <td><?php echo $id;?></td>
+                                    <td><?php echo $name;?></td>
+                                    <td><?php echo $condition;?></td>
+                                    <td>
+                                        <a href="#"><button class = "btn-secondary">Update</button></a>
+                                        <a href="#"><button class = "btn-danger">Delete</button></a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }
+                    else
+                    {
+                        ?>
+                            <tr>
+                                <td colspan = "4">No equipments found</td>
+                            </tr>
+                        <?php
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
+
+<?php include("partials/footer.php");?>
